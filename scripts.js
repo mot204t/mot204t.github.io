ddd598e32682898ade050fd6df62c5efaefa5066
mot204t <121134618+mot204t@users.noreply.github.com>
2023-12-30 00:10:49 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,10 +1,14 @@
-const share = document.querySelector('.share')
-
-
-setTimeout(() => {
-    share.classList.add("hover");
-}, 1000);
-
-setTimeout(() => {
-    share.classList.remove("hover");
-}, 3000);
+const text = document.querySelector(".sec-text");
+        const textLoad = () => {
+            setTimeout(() => {
+                text.textContent = "Freelancer";
+            }, 0);
+            setTimeout(() => {
+                text.textContent = "Blogger";
+            }, 6000);
+            setTimeout(() => {
+                text.textContent = "YouTuber";
+            }, 12000); //1s = 1000 milliseconds
+        }
+        textLoad();
+        setInterval(textLoad, 12000);
